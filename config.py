20c54bbefb80e221c460732b47cdd2d5976d5b1f
undercontr <onder.alkan15@gmail.com>
2020-08-26 23:48:14 +0300

    security

--- config.py
+++ config.py
@@ -5,15 +5,3 @@ class Config(object):
     SECRET_KEY = 'd73626094f1e438e8ec5b07128e71899'
     SQLALCHEMY_DATABASE_URI = 'mysql://test:test@localhost:3306/payment'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    MAIL_SERVER = "smtp.googlemail.com"
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = "webmaster@analitikkimya.com.tr"
-    MAIL_PASSWORD = "Anw6318A."
-    MAIL_DEBUG = False
-    ADMINS = ['webmaster@analitikkimya.com.tr']
-    LANGUAGES = ['en', 'tr']
-    DATA_PER_PAGE = 25
-    RECAPTCHA_PUBLIC_KEY = "6Lf7OVcUAAAAAGiFguM0f62nkEiBGT_trydb5DM2"
-    RECAPTCHA_PRIVATE_KEY = "6Lf7OVcUAAAAAIw-7pk5A5F3VdeGoGzU45KnM56E"
